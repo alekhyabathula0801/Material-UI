e96ce7e012c1a059c3ffc6994246c053914c9619
arun kumar <alekhya0801@gmail.com>
2020-07-19 22:20:56 +0530

    Added : pricing ui information

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Info from './components/Info';
+import './css/pricing.css';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="App">
+      <Info />
     </div>
   );
 }
---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+    render() {
+        return (
+            <div className="info">
+                <span className="pricing">Pricing</span>
+                <p>Quickly build an effective pricing table for your potential <br />
+                customers with this layout. It's built with default <br />
+                Material-UI components with little customization.</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
