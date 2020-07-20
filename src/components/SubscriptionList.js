d31ac031689215c0c0bb9b281e96d7d7c66bed91
arun kumar <alekhya0801@gmail.com>
2020-07-20 21:58:09 +0530

    Added : star icon for pro subscription list

--- src/components/SubscriptionList.js
+++ src/components/SubscriptionList.js
@@ -4,8 +4,14 @@ class SubscriptionList extends Component {
     render() {
         return (
             <div className={this.props.className}>
-                <div id="heading">{this.props.name}
-                <li>{this.props.popular}</li>
+                <div id="heading">
+                    {this.props.name === "Pro" &&
+                        <span id="pro-star">&#9734;</span>
+                    }
+                    {this.props.name}
+                    {this.props.name === "Pro" &&
+                        <li>{this.props.popular}</li>
+                    }
                 </div>
                 <div id="subscription-list-content">
                     <p><span id="cost">${this.props.cost}</span><span id="month">/mo</span></p>
