826fcb30fcb836dd05360fac7776781fa4e3fb33
arun kumar <alekhya0801@gmail.com>
2020-07-20 08:12:49 +0530

    Modified : get started button styling

--- src/components/PricingList.js
+++ src/components/PricingList.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
 class PricingList extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div className={this.props.className}>
