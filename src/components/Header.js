f1376720a02f0ecc1ec242bf35d9a33467933ff0
arun kumar <alekhya0801@gmail.com>
2020-07-22 19:22:51 +0530

    Added : toggle button for login and logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            isLogin: true,
+        }
+    }
+
+    toggleLoginButton() {
+        this.setState(
+            { isLogin: !this.state.isLogin, }
+        );
+    }
+
     render() {
         return (
             <div className="header">
                 <div id="header-company"><h6>Company name</h6></div>
                 <div id="header-menu">
                     <Link to="/subscription">
-                    <a>SUBSCRIPTION</a>
+                        <a>SUBSCRIPTION</a>
                     </Link>
                     <Link to="/user-list">
-                    <a href="#">USER</a>
+                        <a href="#">USER</a>
                     </Link>
                     <a href="#">SUPPORT</a>
-                    <button id="login">LOGIN</button>
+                    <button id="login" onClick={() => this.toggleLoginButton()}>{this.state.isLogin ? "LOGIN" : "LOGOUT"}</button>
                 </div>
             </div>
         );
